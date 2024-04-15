d63fbfa51f00be9ae0b680cf8bb874da88d3a15e
patawang1 <patawang@umich.edu>
2024-04-15 14:32:12 -0400

    fixed transofmration matrix

--- cube.cpp
+++ cube.cpp
@@ -8,11 +8,12 @@
 
 void Cube::reset() {
     points.clear();
-    points.reserve(originalMat.size());   // 8
+    points.reserve(originalMat.size());  // 8
 
     // 2 rows, 3 columns, visualize this transformed
     // Don't know why they have goofy ahh syntax but it's const so it's fast
-    const Eigen::MatrixXd transformation = (Eigen::MatrixXd(3, 3) << 1, 0, 0, 0, 1, 0, 0, 0, 0).finished();
+    const Eigen::MatrixXd transformation =
+        (Eigen::MatrixXd(3, 3) << 1, 0, 0, 0, 1, 0, 0, 0, 0).finished();
 
     project_to_plane(transformation);
 }
@@ -22,11 +23,15 @@ void Cube::transformed_points(const double& x, const double& y) {
     rotY += y;
 
     // transformation matrix rotating along x axis
-    Eigen::MatrixXd rotx_transformation
-      = (Eigen::MatrixXd(3, 3) << 1, 0, 0, 0, cos(rotX), -sin(rotY), 0, sin(rotY), cos(rotY)).finished();
+    Eigen::MatrixXd rotx_transformation =
+        (Eigen::MatrixXd(3, 3) << 1, 0, 0, 0, cos(rotX), -sin(rotX), 0,
+         sin(rotX), cos(rotX))
+            .finished();
     // transformation matrix rotating along y axis
-    Eigen::MatrixXd roty_transformation
-      = (Eigen::MatrixXd(3, 3) << cos(rotY), 0, sin(rotY), 0, 1, 0, -sin(rotY), 0, cos(rotY)).finished();
+    Eigen::MatrixXd roty_transformation =
+        (Eigen::MatrixXd(3, 3) << cos(rotY), 0, sin(rotY), 0, 1, 0, -sin(rotY),
+         0, cos(rotY))
+            .finished();
     // both
     roty_transformation = rotx_transformation * roty_transformation;
 
@@ -39,7 +44,7 @@ void Cube::project_to_plane(const Eigen::MatrixXd& transformation) {
     points.clear();
     for (const Eigen::Vector3d& vec : originalMat) {
         const Eigen::Vector2d pointVector = transformation * vec;
-        points.push_back(Point2D { pointVector[0], pointVector[1] });
+        points.push_back(Point2D{pointVector[0], pointVector[1]});
     }
 }
 
